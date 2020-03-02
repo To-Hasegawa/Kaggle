41e8b68af7d10a73ecaaf267e7cd6bbf91438a5d
To-Hasegawa <hasegawa16777216@gmail.com>
2020-03-02 21:22:26 +0900

    Update drop_high_correlation.py

--- Tools/drop_high_correlation.py
+++ Tools/drop_high_correlation.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+#load data
+train = read.csv("***")
+# call feature engineering function
+features = train.columns
 #Drop high correlation features
 counter = 0
 to_remove = []
 for feat_a in features:
     for feat_b in features:
         if feat_a != feat_b and feat_a not in to_remove and feat_b not in to_remove:
-            c = np.corrcoef(reduce_train[feat_a], reduce_train[feat_b])[0][1]
+            c = np.corrcoef(train[feat_a], train[feat_b])[0][1]
             if c > 0.995:
                 counter += 1
                 to_remove.append(feat_b)
