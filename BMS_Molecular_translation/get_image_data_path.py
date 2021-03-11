4832a941bb52ca9a4fd4e9331e6391f3aef920a9
To-Hasegawa <hasegawa16777216@gmail.com>
2021-03-11 23:47:32 +0900

    Create get_image_data_path.py

---
+++ BMS_Molecular_translation/get_image_data_path.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+train = pd.read_csv('../input/bms-molecular-translation/train_labels.csv')
+test = pd.read_csv('../input/bms-molecular-translation/sample_submission.csv')
+
+def get_train_file_path(image_id):
+    return "../input/bms-molecular-translation/train/{}/{}/{}/{}.png".format(
+        image_id[0], image_id[1], image_id[2], image_id 
+    )
+
+def get_test_file_path(image_id):
+    return "../input/bms-molecular-translation/test/{}/{}/{}/{}.png".format(
+        image_id[0], image_id[1], image_id[2], image_id 
+    )
+
+train['file_path'] = train['image_id'].progress_apply(get_train_file_path)
+test['file_path'] = test['image_id'].progress_apply(get_test_file_path)
