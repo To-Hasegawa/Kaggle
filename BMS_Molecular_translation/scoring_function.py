75640f105bcadec35424823daf9cf82f37230ad3
To-Hasegawa <hasegawa16777216@gmail.com>
2021-03-11 23:47:35 +0900

    Create scoring_function.py

---
+++ BMS_Molecular_translation/scoring_function.py
@@ -0,0 +1,8 @@
+#ref: https://www.kaggle.com/yasufuminakama/molecular-translation-naive-baseline
+def get_score(y_true, y_pred):
+    scores = []
+    for true, pred in zip(y_true, y_pred):
+        score = Levenshtein.distance(true, pred)
+        scores.append(score)
+    avg_score = np.mean(scores)
+    return avg_score
